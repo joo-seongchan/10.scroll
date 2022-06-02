3a526b13c6ca07e92e1f2f5b8486fd00ed99e64b
joo-seongchan <jskjsj601@gmail.com>
2022-06-02 13:50:08 +0900

    page.ex

---
+++ js/pageEvent.js
@@ -0,0 +1,31 @@
+(function(){
+
+    const wrapEl = document.querySelector('.wrap')
+    const gageEl = document.querySelector('.gage')
+
+    const handleGage = () =>{
+
+        const sct = window.pageYOffset
+        // gageEl.style.width = sct +'px'
+        // console.log('페이지 전체 높이값:' , wrapEl.offsetHeight)
+        // console.log('브라우저의 높이 값:', window.innerHeight)
+        // console.log('실제 사용할 수 있는값:', wrapEl.offsetHeight-window.innerHeight)
+        const useScroll = wrapEl.offsetHeight-window.innerHeight
+
+        const per = sct/useScroll*100
+        // console.log(per);
+
+        gageEl.style.width = per +"%"
+
+        // *공식
+        // =>사용할 스크롤값 : 문서의 높이값 - 브라우저의 높이값
+        //         (선택자.offetHeight-window.innerHeight)
+        // =>사용할 스크롤 퍼센트 처리 : 사용할 스크롤/사용할 수 있는 스크롤값 * 100 +'%'
+        //         (pageYoffset/계산해둔 사용할 스크롤 값 * 100)
+
+    }
+
+    window.addEventListener('scroll', handleGage)
+
+
+})()
---
+++ js/style.js
@@ -0,0 +1,11 @@
+(function(){
+
+
+    const boxEl = document.querySelector(".box")
+
+    // boxEl.style.height = '500px'
+    // boxEl.style.borderRadius = '50%'
+
+
+
+})()
